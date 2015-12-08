5d4718f611387ac1397e89ba0ed0a43a52258fda
klaslofstedt <klaslofstedt22@gmail.com>
2015-12-08 18:25:29 +0100

    updated main.c and makefile

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 // lab 6
+// NOT FINISHED
 #include <stdio.h>
 #include "libcomponent.h"
 
 int main(void)
 {
+    // Libresistance test part
+    
+    // libpower test part
+    
+    // libcomponent test part (works like shit by now)
     float res_array[3];
     int count = e_resistance(50, &res_array); 
     printf("count: %d\n", count);
--- makefile
+++ makefile
@@ -1,17 +1,19 @@
 # makefile
+# NOT FINISHED
 
 CC=gcc
 CFLAGS=-c -Wall -I.
 
 all: lab6
 
+# add libpower.o and libresistance.o on this line
 lab6: main.o libcomponent.o 
 	$(CC) main.o libcomponent.o 
 
 clean:
 	rm *o lab6 
-install:
-	cp lab6/bin
+#install:
+#	cp lab6/bin
 
-uninstall:
-	rm /bin/lab6
+#uninstall:
+#	rm /bin/lab6
