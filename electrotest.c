eccfeb8967c4ca5266a5689b01d93bd5825af44a
Andreas Petersson <petersson.andreas@outlook.com>
2015-12-10 21:58:04 +0100

    Added test for libpower

--- electrotest.c
+++ electrotest.c
@@ -2,17 +2,26 @@
 // NOT FINISHED
 #include <stdio.h>
 #include "libcomponent.h"
+#include "libpower.h"
 
 int main(void)
 {
     int count = 3; 
-    int resistance = 1398;
+    float resistance = 1398;
+    float volt = 5;
+    float current = 1.5;
+
     // Libresistance test part
     printf("test phase 1 ------------------------\n");
     
     // libpower test part
     printf("test phase 2 ------------------------\n");
-    
+    printf("Volt = %fV\n", volt);
+    printf("Current = %fA\n", current);
+    printf("Resistance = %fohm\n", resistance);
+    printf("Calculated power(P=U*U/R): %f\n", calc_power_r(volt, resistance));
+    printf("Calculated power(P=U*I): %f\n", calc_power_i(volt, current));
+
     // libcomponent test part 
     printf("test phase 3 ------------------------\n");
     float res_array[3]; 
