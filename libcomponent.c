b98a3fc1bc3f7c6f9dd89374654dd26511532649
klaslofstedt <klaslofstedt22@gmail.com>
2015-12-10 11:57:14 +0100

    lib3 works kinda

--- libcomponent.c
+++ libcomponent.c
@@ -1,49 +1,51 @@
 // libcomponent.c
+#include <stdio.h>
+
 int E12(int temp_resistance, int *c)
 {
     int count = 1; 
     int resistance;
-    if(temp_resistance < 82){
+    if(temp_resistance >= 82){
         resistance = 82;
     }
-    else if(temp_resistance < 68){
+    else if(temp_resistance >= 68){
         resistance = 68;
     }
-    else if(temp_resistance < 56){
+    else if(temp_resistance >= 56){
         resistance = 56;
     }
-    else if(temp_resistance < 47){
+    else if(temp_resistance >= 47){
         resistance = 47;
     }
-    else if(temp_resistance < 39){
+    else if(temp_resistance >= 39){
         resistance = 39;
     }
-    else if(temp_resistance < 33){
+    else if(temp_resistance >= 33){
         resistance = 33;
     }
-    else if(temp_resistance < 27){
+    else if(temp_resistance >= 27){
         resistance = 27;
     }
-    else if(temp_resistance < 22){
+    else if(temp_resistance >= 22){
         resistance = 22;
     }
-    else if(temp_resistance < 18){
+    else if(temp_resistance >= 18){
         resistance = 18;
     }
-    else if(temp_resistance < 15){
+    else if(temp_resistance >= 15){
         resistance = 15;
     }
-    else if(temp_resistance < 12){
+    else if(temp_resistance >= 12){
         resistance = 12;
     }
-    else if(temp_resistance < 10){
+    else if(temp_resistance >= 10){
         resistance = 10;
     }
     else{
         count = 0;
         resistance = 0;
     }
-    *c = count;
+    *c += count;
     return resistance;
 }
 
@@ -63,10 +65,16 @@ int e_resistance(float orig_resistance, float *res_array)
     int i;
     for(i = 0; i < 3; i++){
         int temp_resistance = (int)orig_resistance - left_resistance;
+        //printf("temp_resistance: %d\n", temp_resistance);
         int multiple = multiple_calc(&temp_resistance);
+        //printf("multiple: %d\n", multiple);
         int new_resistance = E12(temp_resistance, &count);
+        //printf("new_resistance: %d\n", new_resistance);
+        //printf("count: %d\n", count);
         left_resistance += new_resistance * multiple;
         res_array[i] = new_resistance * multiple;
+        //printf("res_array: %f\n", res_array[i]);
     }
+    //printf("count_tot: %d\n", count);
     return count;
 }
--- main.c
+++ main.c
@@ -5,17 +5,20 @@
 
 int main(void)
 {
+    int count = 3; 
+    int resistance = 1398;
     // Libresistance test part
     
     // libpower test part
     
     // libcomponent test part (works like shit by now)
-    float res_array[3];
-    int count = e_resistance(50, &res_array); 
-    printf("count: %d\n", count);
+    
+    float res_array[3]; // temporary array until we have a dynamic
+    count = e_resistance(resistance, &res_array[0]); 
     int i;
     for(i = 0; i < 3; i++){
-        printf("res: %d\n", res_array[i]);
+        printf("resistance value: %f\n", res_array[i]);
     }
+    printf("count: %d\n", count);
 	return 0;
 }
