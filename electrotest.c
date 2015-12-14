e2b90df1dd6c4e453d371c5db6b568d5c5b69a5e
klaslofstedt <klaslofstedt22@gmail.com>
2015-12-14 17:22:13 +0100

    style fixes

--- electrotest.c
+++ electrotest.c
@@ -10,12 +10,11 @@
 
 int main(void)
 {
-    int count = 3; 
     float resistance = 1398;
     float volt = 5;
     float current = 1.5;
     char coupling;
-    int numcomponents, i;
+    int count, i;
     float* components;
 
     // Libresistance test part
@@ -24,20 +23,20 @@ int main(void)
     scanf("%c", &coupling);
     coupling = tolower(coupling);
     printf("Amount of components: ");
-    scanf(" %d", &numcomponents);
-    components = malloc(numcomponents * sizeof(float));
-    for(i = 0; i < numcomponents; ++i) {
-    	printf("Component %d in ohm: ", (i + 1));
+    scanf(" %d", &count);
+    components = malloc(count * sizeof(float));
+    for(i = 0; i < count; ++i) {
+    	printf("Component %d in ohm = ", (i + 1));
     	scanf(" %f", &components[i]);
     }
-    printf("Resistance: %g ohm\n", calc_resistance(numcomponents, coupling, components));
+    printf("Resistance = %g ohm\n", calc_resistance(count, coupling, components));
     free(components);
 
     // libpower test part
     printf("test phase 2 ------------------------\n");
-    printf("Volt = %fV\n", volt);
-    printf("Current = %fA\n", current);
-    printf("Resistance = %fohm\n", resistance);
+    printf("Volt = %f V\n", volt);
+    printf("Current = %f A\n", current);
+    printf("Resistance = %f ohm\n", resistance);
     printf("Calculated power(P=U*U/R): %f\n", calc_power_r(volt, resistance));
     printf("Calculated power(P=U*I): %f\n", calc_power_i(volt, current));
 
@@ -45,9 +44,9 @@ int main(void)
     printf("test phase 3 ------------------------\n");
     float res_array[3]; 
     count = e_resistance(resistance, &res_array[0]); 
-    printf("Resistor 1 = %fohm\n", res_array[0]);
-    printf("Resistor 2 = %fohm\n", res_array[1]);
-    printf("Resistor 3 = %fohm\n", res_array[2]);
+    printf("Resistor 1 = %f ohm\n", res_array[0]);
+    printf("Resistor 2 = %f ohm\n", res_array[1]);
+    printf("Resistor 3 = %f ohm\n", res_array[2]);
     printf("Count = %d\n", count);
     return 0;
 }
